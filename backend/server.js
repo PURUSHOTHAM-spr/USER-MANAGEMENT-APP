51293a777682e923df3fb1270134068701715290
PURUSHOTHAM-spr <purushothamshagamreddy464@gmail.com>
2026-03-11 17:14:04 +0530

    fix vercel routing

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(
       "http://localhost:5173",
       "https://user-management-app-gilt-seven.vercel.app"
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
